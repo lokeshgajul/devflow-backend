e79bae7cb0da13fa963fc082db1f42fb44a4ee74
lokeshgajul <sangeetagajul@gmail.com>
2025-12-17 18:15:31 +0530

    added timeout limit

--- db/Db.js
+++ db/Db.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
-export const connectDb = async () => {
+export const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
-    console.log("Connected Successfully");
+    if (!process.env.mongoUri) {
+      throw new Error("Missing mongoUri environment variable");
+    }
+
+    const conn = await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+    });
   } catch (error) {
-    console.log("error occurred ", error.message);
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };
